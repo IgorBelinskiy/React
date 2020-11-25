15fa96ec1c4f0da32499f29bafd749432890ac6d
Igor Belinskiy <i.belinskiy92@gmail.com>
2020-11-25 23:40:20 +0200

    import\export, components

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+   return (<div>
+      it-kamasutra
+   </div>
+   );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+   return <header className='header'>
+      <img src='https://fan-shop.com.ua/imgs/m9277.jpg'></img>
+   </header>;
+}
+
+export default Header;

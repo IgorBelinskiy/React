7ecc53fc96c6becaf58699761a0660938b32a16d
Igor Belinskiy <i.belinskiy92@gmail.com>
2020-11-28 10:02:53 +0200

    page Dialogs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
@@ -7,11 +8,16 @@ import Profile from './components/Profile/Profile';
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <Header/>
+      <Header />
+
+      <Navbar />
+
+      {/* <Profile/> */}
       
-      <Navbar/>
-      
-      <Profile/>
+      <div className='app-wrapper-content'>
+        <Dialogs />
+      </div>
+
 
     </div>
   );

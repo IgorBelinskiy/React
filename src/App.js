a97619bbf26d0e78e62c3bb553c5e3aaf1312569
Igor Belinskiy <i.belinskiy92@gmail.com>
2020-11-28 12:06:30 +0200

    pages News, Music, Settings

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
+import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
+import News from './components/News/News';
 import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Setting';
 
 const App = () => {
   return (
@@ -15,6 +18,9 @@ const App = () => {
         <div className='app-wrapper-content'>
           <Route path='/dialogs' component={Dialogs} />
           <Route path='/profile' component={Profile} />
+          <Route path='/music' component={Music} />
+          <Route path='/news' component={News} />
+          <Route path='/settings' component={Settings} />
         </div>
       </div>
     </BrowserRouter>

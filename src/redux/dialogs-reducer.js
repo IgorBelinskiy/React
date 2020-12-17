960543add72a6c0ac4a9ec4ec211ac2a6a6402a2
IgorBelinskiy <i.belinskiy92@gmail.com>
2020-12-17 22:50:27 +0200

    redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,25 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+   dialogs: [
+      { id: 1, name: 'Igor' },
+      { id: 2, name: 'Katya' },
+      { id: 3, name: 'Vanya' },
+      { id: 4, name: 'Roma' },
+      { id: 5, name: 'Petsa' },
+      { id: 6, name: 'Sanya' }
+   ],
+   messages: [
+      { id: 1, message: 'Hi' },
+      { id: 2, message: 'How is your it-kamasutra' },
+      { id: 3, message: 'Yo' },
+      { id: 4, message: 'Yo-Yo' }
+   ],
+   newMessageBody: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
    switch (action.type) {
       case UPDATE_NEW_MESSAGE_BODY:
          state.newMessageBody = action.newBody;
